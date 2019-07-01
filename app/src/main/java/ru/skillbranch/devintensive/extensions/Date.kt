67e2f07098441e51e07284560254c6691eadf27c
Vladislav Larin <egoludens@yandex.ru>
2019-07-02 01:36:21 +0300

    Date.humanizeDiff implemented (Test 8 for assignment 2)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.roundToInt
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -25,6 +26,81 @@ fun Date.add(value: Int, units: TimeUnits): Date
     }
     this.time = time
     return this
+}
+
+fun numeralRu(value: Int, single: String, few: String, many: String): String
+{
+    return when {
+        value % 10 == 0 -> many
+        value in 11..19 -> many
+        value % 10 == 1 -> single
+        value % 10 in 1..4 -> few
+        else -> many
+    }
+}
+
+
+fun Date.humanizeDiff(): String
+{
+    val diffMilsec:Long = Date().time - this.time
+    return when {
+        diffMilsec > 360 * DAY -> "более года назад"
+        diffMilsec < -360 * DAY -> "более чем через год"
+        diffMilsec > 26 * HOUR -> {
+            val days: Int = (diffMilsec.toFloat() / DAY.toFloat()).roundToInt()
+            "$days ${numeralRu(days, "день", "дня", "дней")} назад"
+        }
+        diffMilsec < -26 * HOUR -> {
+            val days: Int = (diffMilsec.toFloat() / DAY.toFloat()).roundToInt()
+            "через ${-days} ${numeralRu(-days, "день", "дня", "дней")}"
+        }
+        diffMilsec > 22 * HOUR -> "день назад"
+        diffMilsec < -22 * HOUR -> "через день"
+        diffMilsec > 75 * MINUTE -> {
+            val hours: Int = (diffMilsec.toFloat() / HOUR.toFloat()).roundToInt()
+            "$hours ${numeralRu(hours, "час", "часа", "часов")} назад"
+        }
+        diffMilsec < -75 * MINUTE -> {
+            val hours: Int = (diffMilsec.toFloat() / HOUR.toFloat()).roundToInt()
+            "через ${-hours} ${numeralRu(-hours, "час", "часа", "часов")}"
+        }
+        diffMilsec > 45 * MINUTE -> "час назад"
+        diffMilsec < -45 * MINUTE -> "через час"
+        diffMilsec > 75 * SECOND -> {
+            val minutes: Int = (diffMilsec.toFloat() / MINUTE.toFloat()).roundToInt()
+            "$minutes ${numeralRu(minutes, "минута", "минуты", "минут")} назад"
+        }
+        diffMilsec < -75 * SECOND -> {
+            val minutes: Int = (diffMilsec.toFloat() / MINUTE.toFloat()).roundToInt()
+            "через ${-minutes} ${numeralRu(-minutes, "минута", "минуты", "минут")}"
+        }
+        diffMilsec > 45 * SECOND -> "минуту назад"
+        diffMilsec < -45 * SECOND -> "через минуту"
+        diffMilsec > 1 * SECOND -> "несколько секунд назад"
+        diffMilsec < -1 * SECOND -> "через несколько секунд"
+        else -> "только что"
+    }
+
+//    0с - 1с "только что"
+//
+//    1с - 45с "несколько секунд назад"
+//
+//    45с - 75с "минуту назад"
+//
+//    75с - 45мин "N минут назад"
+//
+//    45мин - 75мин "час назад"
+//
+//    75мин 22ч "N часов назад"
+//
+//    22ч - 26ч "день назад"
+//
+//    26ч - 360д "N дней назад"
+//
+//    >360д "более года назад"
+
+
+
 }
 
 enum class TimeUnits {
--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -3,10 +3,7 @@ package ru.skillbranch.devintensive
 import org.junit.Test
 
 import org.junit.Assert.*
-import ru.skillbranch.devintensive.extensions.SECOND
-import ru.skillbranch.devintensive.extensions.TimeUnits
-import ru.skillbranch.devintensive.extensions.add
-import ru.skillbranch.devintensive.extensions.format
+import ru.skillbranch.devintensive.extensions.*
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
@@ -79,5 +76,16 @@ class ExampleUnitTest {
     {
         println(Utils.transliteration("Женя Стереотипов")) //Zhenya Stereotipov
         println(Utils.transliteration("Amazing Петр","_")) //Amazing_Petr
+    }
+
+    @Test
+    fun hometask_2_Date_humanizeDiff_works()
+    {
+        println(Date().add(-2, TimeUnits.HOUR).humanizeDiff()) //2 часа назад
+        println(Date().add(-5, TimeUnits.DAY).humanizeDiff()) //5 дней назад
+        println(Date().add(2, TimeUnits.MINUTE).humanizeDiff()) //через 2 минуты
+        println(Date().add(7, TimeUnits.DAY).humanizeDiff()) //через 7 дней
+        println(Date().add(-400, TimeUnits.DAY).humanizeDiff()) //более года назад
+        println(Date().add(400, TimeUnits.DAY).humanizeDiff()) //более чем через год
     }
 }
